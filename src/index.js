f42d7378b82ea4631a551b4aeace5bf8adead6ea
DuongHoangR1pper <duonghoangripper25@gmail.com>
2024-08-17 13:08:24 +0700

    check 1

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-const path = require('path');
-const express = require('express');
-const handlebars = require('express-handlebars');
-const morgan = require('morgan');
-const { log } = require('console');
+const path = require("path");
+const express = require("express");
+const handlebars = require("express-handlebars");
+const morgan = require("morgan");
+const { log } = require("console");
 
 const app = express();
 const port = 3000;
-const route = require('./routes');
+const route = require("./routes");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // app.use(morgan("combined"));
 app.engine(
-    'hbs',
+  "hbs",
 
-    handlebars.engine({
-        extname: '.hbs',
-    }),
+  handlebars.engine({
+    extname: ".hbs"
+  })
 );
-    app.set('view engine', 'hbs');
-app.set('views',        path.join(__dirname, 'resources', 'views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
 
-    route(app);
+route(app);
 
 app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
